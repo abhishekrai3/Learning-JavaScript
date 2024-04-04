b38384415f21f2c3a72d9e934ec1f1a357899642
Abhishek Rai <62717502+abhishekrai3@users.noreply.github.com>
2024-04-04 21:36:04 +0000

    using dataType

---
+++ Let-Var-Const/dataType.js
@@ -0,0 +1,31 @@
+"use strict" // treat all js code as unique version but now every code considerd a unique version
+// alert(3+3) it is not working because we are using node js not browser
+
+//code readabiliy should be high in a code  emca write standard for javascript code
+
+// Datatype --> Primitive DataType   --> Non  Primitive Object 
+//1 number int 2 power 52 and than go to bigInt which doesnot largely used in this 
+let a=32423432
+
+// 2 string it is store in " "
+let b="abhishek"
+
+// 3 boolean which have true and false value
+let c= true
+// we can change the dataype of any variable in a program
+
+//4
+c=1
+
+//4 null it is a standalone value in javaScriopt
+let d= null
+
+//5. undefined is a basically a type of value  valuein it
+let f;
+
+//6 Symbol is unique there are various unique symbol used in it
+
+// object is primitive datatype
+
+console.log(typeof d)
+console.log(typeof f)
