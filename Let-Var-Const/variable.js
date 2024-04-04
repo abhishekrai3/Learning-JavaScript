646ddc3040156a02d7bc383d92eac5276cc8eaf9
Abhishek Rai <62717502+abhishekrai3@users.noreply.github.com>
2024-04-04 21:12:56 +0000

    learnig var , const , let keyword

---
+++ Let-Var-Const/variable.js
@@ -0,0 +1,21 @@
+
+// const value cant be change in a program
+const password=123456
+
+// let and var value can be changable
+let name="Abhishek Rai"
+var address="h no 529 ganaga nagar"
+ 
+// variable declartion can be done with out keyword
+work="professor"
+
+// now a days we dont have to use var keyword because of scope problem it can easily change the value anywhere
+// always use let keyword here we access variable according to scopes of it
+
+// variable not defined than return undefined value
+let state;
+
+name="Rai"
+
+address="h no 49 kamla nagar"
+console.table([password, name , address, work, state]);
